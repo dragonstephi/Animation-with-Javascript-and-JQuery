7dab68d81e14e3f58aa17d0cca239b2319e99da4
Yongmin Lee <89743288+dragonstephi@users.noreply.github.com>
2021-11-28 00:54:56 +0900

    Add files via upload

---
+++ WEB1091_js-tabs-start/script.js
@@ -0,0 +1,18 @@
+var tabsLink = document.querySelectorAll('#tabs > ul > li a');
+
+
+for(var i=0; i<tabsLink.length; i++){
+    tabsLink[i].addEventListener('click', function(event){
+        event.preventDefault();
+        var hrefLink = document.querySelectorAll('#href');
+        alert(hrefLink.innerHTML);
+        if (!this.classList.contains('active')){
+            this.classList.add('active');
+        } else {
+            
+        }
+        //this.classList.add('active');
+        
+    });
+};
+console.log(tabsLink.innerHTML);
