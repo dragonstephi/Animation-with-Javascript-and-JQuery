6653e84b19a9f3782eef90a0cf3264b695516fdc
Yongmin Lee <89743288+dragonstephi@users.noreply.github.com>
2021-12-01 11:17:03 +0900

    Update script.js

--- WEB1091_js-tabs-start/script.js
+++ WEB1091_js-tabs-start/script.js
@@ -1,17 +1,112 @@
-var tabsLink = document.querySelectorAll('#tabs > ul > li a');
+(function(){
+    'use strict';
 
+    const tabs = document.querySelectorAll('#tabs > ul > li > a');
 
-for(var i=0; i<tabsLink.length; i++){
-    tabsLink[i].addEventListener('click', function(event){
-        event.preventDefault();
-        var hrefLink = document.querySelectorAll('#href');
-        alert(hrefLink.innerHTML);
-        if (!this.classList.contains('active')){
-            this.classList.add('active');
-        } else {
-            
-        }
-       
+    tabs.forEach(tab => {
+        tab.addEventListener('click', selectTab);
     });
-};
-console.log(tabsLink.innerHTML);
+
+    function selectTab(event){
+        event.preventDefault();
+        tabs.forEach(tab => {
+            tab.removeAttribute('class');
+        });
+        event.target.className = 'active';
+
+        const thisTab = this.getAttribute('href');
+        const thisContent = document.querySelector(thisTab);
+
+        const oldContent = document.querySelector('.visible');
+        oldContent.className = 'visuallyhidden';
+        oldContent.addEventListener('transitionend', function(){
+            oldContent.className = 'hidden';
+            thisContent.className = 'visuallyhidden visible';
+            setTimeout(function(){
+                thisContent.classList.remove('visuallyhidden');
+            }, 300);
+        },{capture: false, once: true, passive: false});
+    };
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+    const tabs = document.querySelectorAll('#tabs > ul > li > a');
+
+    for (var i=0; i<tabs.length; i++){
+        tabs[i].addEventListener('click', selectTab);
+    }
+
+    function selectTab(event){
+        event.preventDefault();
+
+        for (var i=0; i<tabs.length; i++){
+            tabs[i].removeAttribute('class');
+        };
+
+        event.target.classList.add('active');
+
+        var thisTab = event.target.getAttribute('href');
+        var thisContent = document.querySelector(thisTab);
+    
+        var oldContent = document.querySelector('.visible');
+        oldContent.className = 'visuallyhidden';
+        oldContent.addEventListener('transitionend', function(){
+            oldContent.className = 'hidden';
+            thisContent.className = 'visuallyhidden visible';
+            setTimeout(function(){
+                thisContent.classList.remove('visuallyhidden');
+            }, 20);
+        },{capture:false, once:true, passive:false});
+    }; */
