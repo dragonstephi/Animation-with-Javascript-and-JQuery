526d8667d0e21e5d4b3223a7413a7ad46793871b
Yongmin Lee <89743288+dragonstephi@users.noreply.github.com>
2021-12-10 00:16:23 +0900

    Add files via upload

---
+++ WEB1092_Scroll-Effects-START/script.js
@@ -0,0 +1,33 @@
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(function(eachLink){
+    eachLink.addEventListener('click', smoothScroll);
+});
+
+function smoothScroll(evt){
+    evt.preventDefault();
+    const targetID = event.target.getAttribute('href');
+    const targetSection = document.querySelector(targetID);
+    const originalTop = Math.floor(targetSection.getBoundingClientRect().top - 200);
+    window.scrollBy({top: originalTop, left: 0, behavior: 'smooth'});
+    console.log(originalTop);
+};
+
+window.addEventListener('load', function(){
+    const posts = document.querySelectorAll('section');
+    let postTops = [];
+    let pagetop;
+    let counter = 1;
+    let prevcounter =1;
+    let doneResizing;
+    
+    posts.forEach(function(post){
+        postTops.push( Math.floor(post.getBoundingClientRect().top + window.pageYOffset ) );
+    });
+    //console.log(postTops);
+
+    window.addEventListener('scroll', function(){
+        pagetop = window.pageYOffset;
+        console.log(pagetop) + 250;
+    });
+});
